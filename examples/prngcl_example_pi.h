33737e64bf636f3a3184b3c7ee72756e33328a12
Vadim Demchik <vadimdi@yahoo.com>
2014-01-12 01:07:00 +0200

    example: pi calculation

---
+++ examples/prngcl_example_pi.h
@@ -0,0 +1,46 @@
+/******************************************************************************
+ * @file     prngcl_example_pi.h
+ * @author   Vadim Demchik <vadimdi@yahoo.com>
+ * @version  1.1
+ *
+ * @brief    [PRNGCL library]
+ *           Pseudo-random number generators for HGPU package
+ *           [EXAMPLE]: PI calculation
+ *
+ *
+ * @section  LICENSE
+ *
+ * Copyright (c) 2013, Vadim Demchik
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *    Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ *****************************************************************************/
+
+#ifndef PRNGCL_EXAMPLE_PI_H
+#define PRNGCL_EXAMPLE_PI_H
+
+#define EXAMPLE_SRC  "examples/prngcl_example_pi.cl"
+
+                             void   HGPU_GPU_example_pi(int argc, char** argv);
+
+#endif
